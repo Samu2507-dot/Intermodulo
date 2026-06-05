ec8645dbe3c40bb2f2a543d653b2b4bd69915ab3
paulasarnago <gestionespaulasar@gmail.com>
2026-06-05 16:30:24 +0200

    clases (servicios) con test que pasan

---
+++ Intermodular/src/test/java/servicios/MantenimientoServicioTest.java
@@ -0,0 +1,26 @@
+package servicios;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MantenimientoServicioTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void solicitarMantenimiento() {
+    }
+
+    @Test
+    void actualizarEstadoMantenimiento() {
+    }
+}
---
+++ Intermodular/src/test/java/servicios/UsuarioServicioTest.java
@@ -0,0 +1,30 @@
+package servicios;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UsuarioServicioTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void registrarAnfitrion() {
+    }
+
+    @Test
+    void registrarHuesped() {
+    }
+}
