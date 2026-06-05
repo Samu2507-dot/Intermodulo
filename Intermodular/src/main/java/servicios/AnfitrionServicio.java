42c756c81f6be84fae67c846ec726ef78fb85ddf
paulasarnago <gestionespaulasar@gmail.com>
2026-06-05 15:48:58 +0200

    se añade dependencia jbcrypt al pom

---
+++ Intermodular/src/main/java/servicios/AnfitrionServicio.java
@@ -0,0 +1,22 @@
+package servicios;
+
+import entidades.Alojamiento;
+import jakarta.persistence.EntityManager;
+
+public class AnfitrionServicio {
+
+    //Esta clase definira la lógica de las acciones que podrá realizar el anfitrión
+
+
+    private EntityManager em;
+
+    public AnfitrionServicio(EntityManager em){
+        this.em = em;
+    }
+
+    // El anfitrión quiere crear un anuncio
+
+    public Alojamiento crearAlojamiento()
+
+
+}
